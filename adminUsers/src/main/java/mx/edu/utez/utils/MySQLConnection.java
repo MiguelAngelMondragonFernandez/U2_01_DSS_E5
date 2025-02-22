b4633f8ab3d47c40a95a50b0c44c8979f20096c6
MiguelAngelMondragonFernandez <20223tn026@utez.edu.mx>
2025-02-21 18:02:29 -0600

    [ADD] conexion con base de datos

---
+++ adminUsers/src/main/java/mx/edu/utez/utils/MySQLConnection.java
@@ -0,0 +1,33 @@
+package mx.edu.utez.utils;
+
+import java.sql.*;
+
+public class MySQLConnection {
+
+    Connection conn;
+    public static void main(String[] args) {
+        try {
+            Connection con = new MySQLConnection().getConnection();
+            if(con != null){
+                System.out.println("Hello Unhappy");
+                con.close();
+            }else {
+                System.out.println("Sí jaló");
+            }
+        }catch (SQLException e){
+            System.out.println("Eror en: "+e);
+        }
+    }
+
+    public MySQLConnection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/adminusers","root","root");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Error en: "+e);
+        }
+    }
+    public Connection getConnection(){
+        return conn;
+    }
+}
