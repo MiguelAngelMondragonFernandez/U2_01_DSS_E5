f8fb297166f04b9f1c953d29000db455428875d6
MiguelAngelMondragonFernandez <20223tn026@utez.edu.mx>
2025-02-23 00:32:13 -0600

    [UPDATE] refactorizacion en codigo

--- src/main/java/mx/edu/utez/model/Login/LoginDao.java
+++ src/main/java/mx/edu/utez/model/Login/LoginDao.java
@@ -1,7 +1,8 @@
 package mx.edu.utez.model.Login;
+
 import jakarta.servlet.http.HttpSession;
-import mx.edu.utez.adminusers.HelloServlet;
 import mx.edu.utez.utils.MySQLConnection;
+
 import java.sql.*;
 
 
@@ -11,40 +12,60 @@ public class LoginDao {
     public static PreparedStatement pstm;
     public static ResultSet rs;
 
-    public static boolean login(LoginBean loginBean, HttpSession session){
+    public static boolean login(LoginBean loginBean, HttpSession session) {
         String sqlQuery = "SELECT * FROM usuarios where correo = ? and contrasena = sha1(?);";
-        String sqlBitacora = "INSERT INTO bitacora VALUES (null, ?, ?, CURDATE());";
-        try{
+        try {
             connection = new MySQLConnection();
             con = connection.getConnection();
             pstm = con.prepareStatement(sqlQuery);
             pstm.setString(1, loginBean.mail);
             pstm.setString(2, loginBean.pass);
             rs = pstm.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
+                int id = rs.getInt("id");
                 //Se asigna el permiso
-                String permisos = rs.getInt("id") == 1 ? "admin" : "user";
+                String permisos = id == 1 ? "admin" : "user";
                 //Se asigna la sesion con el permiso
-                session.setAttribute("session", loginBean.mail+"/"+loginBean.pass+"/"+permisos);
+                session.setAttribute("session", loginBean.mail + "/" + loginBean.pass + "/" + permisos);
                 //Se registra en la bitacora
-                pstm = con.prepareStatement(sqlBitacora);
-                pstm.setInt(1, rs.getInt("id"));
-                pstm.setString(2, "Metodo POST");
-                long res = pstm.executeUpdate();
+                boolean res = addBitacora(id, "POST");
                 //Se retorna true si la sesion se asigno correctamente y se hizo el registro en la bitacora
-                return session.getAttribute("session") != null && res > 0;
+                return session.getAttribute("session") != null && res;
             }
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         } finally {
-            try {
-                rs.close();
-                pstm.close();
-                con.close();
-            } catch (SQLException e) {
-                System.out.println("Error closing resources: " + e.getMessage());
-            }
+            closeConnection();
         }
         return false;
     }
+
+    public static boolean addBitacora(int idUser, String method) {
+        String sqlQuery = "INSERT INTO bitacora values(null, ?, ?, CURRENT_TIMESTAMP());";
+        String descripcion = "Metodo " + method;
+        try {
+            con = connection.getConnection();
+            pstm = con.prepareStatement(sqlQuery);
+            pstm.setInt(1, idUser);
+            pstm.setString(2, descripcion);
+            int res = pstm.executeUpdate();
+            return res > 0;
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            closeConnection();
+        }
+        return false;
+    }
+
+    public static void closeConnection() {
+        try {
+            rs.close();
+            pstm.close();
+            con.close();
+        } catch (SQLException e) {
+            System.out.println("Error closing resources: " + e.getMessage());
+        }
+    }
+
 }
